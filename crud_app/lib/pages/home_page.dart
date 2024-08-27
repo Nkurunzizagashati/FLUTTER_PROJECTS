b39b0a59e82fbf3eb8bf8aab98bfe4aa0387010c
Fabrice <f.nkurunziz@alustudent.com>
2024-08-27 10:21:56 +0200

    add a function to show a text field dialogue when a button is clicked

--- crud_app/lib/pages/home_page.dart
+++ crud_app/lib/pages/home_page.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  void openNoteBox() {
+    showDialog(
+      context: context,
+      builder: (context) => const AlertDialog(
+        content: TextField(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +27,7 @@ class HomePage extends StatelessWidget {
         foregroundColor: Colors.white,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: openNoteBox,
         child: const Icon(Icons.add),
       ),
     );
