a8ff03ce9688e14b45b46b5a1b53bd05770b3622
Fabrice <f.nkurunziz@alustudent.com>
2024-08-28 10:19:11 +0200

    add functionality to check if the user is authenticated so that they can't access the registration page

--- simple_socialmedia_app/lib/pages/register_page.dart
+++ simple_socialmedia_app/lib/pages/register_page.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_socialmedia_app/helpers/helper_functions.dart';
+import 'package:simple_socialmedia_app/pages/home_page.dart';
+import 'package:simple_socialmedia_app/services/auth_servces.dart';
 // import 'package:flutter/widgets.dart';
 import 'package:simple_socialmedia_app/widgets/button.dart';
 import 'package:simple_socialmedia_app/widgets/text_field.dart';
@@ -20,6 +22,8 @@ class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController passwordFieldController = TextEditingController();
   final TextEditingController consfirmPasswordFieldController =
       TextEditingController();
+
+  final authServices = AuthServices();
 
   void registerUser() async {
     // show loading circle
@@ -43,10 +47,15 @@ class _RegisterPageState extends State<RegisterPage> {
       displayMessageToUser("Passwords don't match", context);
     } else {
       try {
-        UserCredential? userCredential = await FirebaseAuth.instance
-            .createUserWithEmailAndPassword(
-                email: emailFieldController.text,
-                password: passwordFieldController.text);
+        // create user
+
+        await authServices.registerUser(
+            emailFieldController.text, passwordFieldController.text);
+
+        // store user data in the firestore
+
+        await authServices.addUserDetails(emailFieldController.text,
+            passwordFieldController.text, usernameFieldController.text);
 
         // pop loading circle
         Navigator.pop(context);
@@ -73,75 +82,83 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      body: Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(25.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.person,
-                size: 80,
-                color: Theme.of(context).colorScheme.inversePrimary,
-              ),
-              const SizedBox(height: 25),
-              const Text(
-                "M I N I M A L",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              const SizedBox(height: 25),
-              CustomTextField(
-                hintText: "Username",
-                obsecureText: false,
-                controller: usernameFieldController,
-              ),
-              const SizedBox(height: 10),
-              CustomTextField(
-                hintText: "Email",
-                obsecureText: false,
-                controller: emailFieldController,
-              ),
-              const SizedBox(height: 10),
-              CustomTextField(
-                hintText: "Password",
-                obsecureText: true,
-                controller: passwordFieldController,
-              ),
-              const SizedBox(height: 10),
-              CustomTextField(
-                hintText: "Confirm Password",
-                obsecureText: true,
-                controller: consfirmPasswordFieldController,
-              ),
-              const SizedBox(height: 25),
-              CustomButton(
-                text: "register",
-                onTap: registerUser,
-              ),
-              const SizedBox(height: 25),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("Already have an account?"),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushNamed(context, "/login");
-                    },
-                    child: const Text(
-                      " Login Here",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
+      body: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const HomePage();
+            } else {
+              return Center(
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.person,
+                        size: 80,
+                        color: Theme.of(context).colorScheme.inversePrimary,
                       ),
-                    ),
+                      const SizedBox(height: 25),
+                      const Text(
+                        "M I N I M A L",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      const SizedBox(height: 25),
+                      CustomTextField(
+                        hintText: "Username",
+                        obsecureText: false,
+                        controller: usernameFieldController,
+                      ),
+                      const SizedBox(height: 10),
+                      CustomTextField(
+                        hintText: "Email",
+                        obsecureText: false,
+                        controller: emailFieldController,
+                      ),
+                      const SizedBox(height: 10),
+                      CustomTextField(
+                        hintText: "Password",
+                        obsecureText: true,
+                        controller: passwordFieldController,
+                      ),
+                      const SizedBox(height: 10),
+                      CustomTextField(
+                        hintText: "Confirm Password",
+                        obsecureText: true,
+                        controller: consfirmPasswordFieldController,
+                      ),
+                      const SizedBox(height: 25),
+                      CustomButton(
+                        text: "register",
+                        onTap: registerUser,
+                      ),
+                      const SizedBox(height: 25),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Already have an account?"),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushNamed(context, "/login");
+                            },
+                            child: const Text(
+                              " Login Here",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
                   ),
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
+                ),
+              );
+            }
+          }),
     );
   }
 }
