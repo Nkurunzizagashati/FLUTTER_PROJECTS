7c7da4f2e5c3d18d78768a591cfb3011fe3aa9d3
Fabrice <f.nkurunziz@alustudent.com>
2024-09-05 16:52:02 +0200

    login page

---
+++ workflow_management_software/lib/pages/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("L O G I N"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
